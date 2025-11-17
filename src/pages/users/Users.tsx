d15c49e5f3c72c71f2a99354afc2c8b3418f583e
김재희 <kimjaehee@hecto.co.kr>
2025-11-17 16:49:22 +0900

    Users 페이지 자동 생성 테스트

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import {
+    Box,
+    Stack,
+    Button,
+    TableContainer,
+    Paper,
+    Table,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
+} from '@mui/material';
+import { Add } from '@mui/icons-material';
 
 // Import page-specific types
 import { UsersPageState } from './Users.types';
@@ -19,14 +31,68 @@ export const Users: React.FC = () => {
                     gap: 2,
                 }}
             >
-                <Typography
-                    variant="body1"
+                <Stack
+                    direction="row"
+                    spacing={10}
+                    alignItems="center"
+                    justifyContent="space-between"
                     sx={{
-                        color: 'text.primary',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
                     }}
                 >
-                    메인 콘텐츠 영역입니다.
-                </Typography>
+                    <Button variant="outlined" color="secondary">
+                        Outline Button
+                    </Button>
+                    <Button variant="contained" startIcon={<Add />}>
+                        새 계정
+                    </Button>
+                </Stack>
+                <TableContainer
+                    component={Paper}
+                    elevation={0}
+                    variant="outlined"
+                    sx={{
+                        backgroundColor: 'background.paper-elevation-0',
+                    }}
+                >
+                    <Table size="small">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>번호</TableCell>
+                                <TableCell>이름</TableCell>
+                                <TableCell>이메일</TableCell>
+                                <TableCell>소속</TableCell>
+                                <TableCell>권한</TableCell>
+                                <TableCell>상태</TableCell>
+                                <TableCell>등록일</TableCell>
+                                <TableCell>최근 로그인</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>2</TableCell>
+                                <TableCell>홍길동</TableCell>
+                                <TableCell>hecto@hecto.co.kr</TableCell>
+                                <TableCell>(주)헥토</TableCell>
+                                <TableCell>시스템관리자</TableCell>
+                                <TableCell>활성</TableCell>
+                                <TableCell>2025.01.01</TableCell>
+                                <TableCell>2025.01.02</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>1</TableCell>
+                                <TableCell>이순신</TableCell>
+                                <TableCell>hectodata@hecto.co.kr</TableCell>
+                                <TableCell>(주)헥토데이터</TableCell>
+                                <TableCell>일반사용자</TableCell>
+                                <TableCell>중지</TableCell>
+                                <TableCell>2025.01.01</TableCell>
+                                <TableCell>2025.01.01</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </Box>
         </Box>
     );
